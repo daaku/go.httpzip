059b1b97399a218c38fbce36823e1e78b8acd97f
Naitik Shah <n@daaku.org>
2012-04-09 19:03:08 -0700

    initial commit

---
+++ httpzip.go
@@ -0,0 +1,28 @@
+package httpzip
+
+import (
+	"fmt"
+	"archive/zip"
+	"net/http"
+	"bytes"
+	"io/ioutil"
+)
+
+func Read(res *http.Response) (*zip.Reader, error) {
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read response body with error %s", err)
+	}
+	r := bytes.NewReader(b)
+	return zip.NewReader(r, int64(r.Len()))
+}
+
+func ReadURL(url string) (*zip.Reader, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"Fetching zip URL %s failed with error %s.", url, err)
+	}
+	defer res.Body.Close()
+	return Read(res)
+}
